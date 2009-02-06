a58f4cd212113bd4c5feaa6d2578bf46d3a64548
g.helenchen <g.helenchen@b57e6fc4-c6dd-11dd-a3c7-fda619b15ae3>
2009-02-06 17:38:31 +0000

    try transformer url call

--- bin/swo.bat
+++ bin/swo.bat
@@ -1,3 +1,17 @@
-@echo off
+REM @echo off
+REM set dir=%~dp0
+REM wget -nv -O- %1 | %dir%SWtransformer.exe -q - %2 | %dir%SWtransformer.exe -q - %3 -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
+REM cat %1 | %dir%SWtransformer.exe -q - %2 | %dir%SWtransformer.exe -q - %3 -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
+
 set dir=%~dp0
-cat %1 | %dir%SWtransformer -q - %2 | %dir%SWtransformer -q - %3 -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
+set temp=%dir%temp\
+
+wget -nv -O %temp%one %1
+REM wget -nv %2 -O %temp%two
+REM wget -nv %3 -O %temp%three
+
+REM cat %temp%one | %dir%SWtransformer -q - %dir%two | %dir%SWtransformer -q - %dir%three -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
+
+REM del %dir%one
+REM del %dir%two
+REM del %dir%three
