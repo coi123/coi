dfb1c07c2e15b0573cf6babd97d5dc809336acf6
holger.stenzhorn <holger.stenzhorn@b57e6fc4-c6dd-11dd-a3c7-fda619b15ae3>
2009-02-09 17:15:40 +0000

    Checking in changes for Helen

--- trunk/bin/swo.bat
+++ trunk/bin/swo.bat
@@ -1,17 +1,18 @@
-REM @echo off
-REM set dir=%~dp0
-REM wget -nv -O- %1 | %dir%SWtransformer.exe -q - %2 | %dir%SWtransformer.exe -q - %3 -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
-REM cat %1 | %dir%SWtransformer.exe -q - %2 | %dir%SWtransformer.exe -q - %3 -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
-
+@echo off
 set dir=%~dp0
 set temp=%dir%temp\
+for /f "tokens=1,2,3,4,5,6,7,8 delims=.:,\ " %%a in ("%date% %time% %random%") do set "suffix=%%c%%b%%a-%%d%%e%%f%%g-%%h"
 
-wget -nv -O %temp%one %1
-REM wget -nv %2 -O %temp%two
-REM wget -nv %3 -O %temp%three
+\set one=%temp%sdtm-%suffix%.rq 
+\set two=%temp%hl7_sdtm-%suffix%.rq 
+\set three=%temp%db_hl7-%suffix%.rq
 
-REM cat %temp%one | %dir%SWtransformer -q - %dir%two | %dir%SWtransformer -q - %dir%three -s http://hospital.example/DB/ | mysql -u %4 --password="%5" %6 --table
+\wget -nv -O %one% %1
+\wget -nv -O %two% %2
+\wget -nv -O %three% %3
 
-REM del %dir%one
-REM del %dir%two
-REM del %dir%three
+\%dir%SWtransformer -q %one% %two% | %dir%SWtransformer -q - %three% -s http://hospital.example/DB/ | mysql -u %4 --password=%5 %6 --table
+
+\del %one%
+\del %two%
+\del %three%
--- trunk/src/coiP.js
+++ trunk/src/coiP.js
@@ -50,12 +50,152 @@ function getList(n3) {
 	return statements;
 }
 
+function getSDTM()
+{
+	var s = "";
+
+	s += "PREFIX sdtm: <http://www.sdtm.org/vocabulary#> \n"  +  
+		 "PREFIX spl: <http://www.hl7.org/v3ballot/xml/infrastructure/vocabulary/vocabulary#> \n" +
+		 "SELECT ?patient ?dob ?sex ?takes ?indicDate # ?indicEnd ?contra\n" + 
+ 		 "WHERE { \n" +
+  		 "?patient a sdtm:Patient ; \n" +
+         "sdtm:middleName ?middleName ; \n" +
+         "sdtm:dateTimeOfBirth ?dob ; \n" +
+         "sdtm:sex ?sex . \n" +
+  		 "[	  sdtm:subject ?patient ; \n" +
+	     "sdtm:standardizedMedicationName ?takes ; \n" +
+	     "spl:activeIngredient [ spl:classCode 6809 ] ;\n" +
+         "sdtm:startDateTimeOfMedication ?indicDate\n" +
+         "] .\n" +
+         "} LIMIT 30 \n" ;
+    alert(s);
+	return s;
+} 
+
+function getHL7_SDTM()
+{
+	var s = "";
+	s += "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" + 
+			 "PREFIX hl7: <http://www.hl7.org/v3ballot/xml/infrastructure/vocabulary/vocabulary#> \n" + 
+			 "PREFIX spl: <http://www.hl7.org/v3ballot/xml/infrastructure/vocabulary/vocabulary#> \n" + 
+			 "PREFIX sdtm: <http://www.sdtm.org/vocabulary#> \n" + 
+			 "PREFIX bridg: <> \n" + 
+			 "PREFIX caBIG: <> \n" + 
+			 "PREFIX CDASH: <> \n" + 
+			 "PREFIX MHONG: <> \n" + 
+			 "PREFIX dam: <> \n" + 
+			 "CONSTRUCT { \n" + 
+			 "?patient \n" + 
+    			 "a					 		sdtm:Patient ; \n" + 
+    			 "sdtm:middleName			?middleName ; \n" + 
+    			 "sdtm:dateTimeOfBirth		?dob ; \n" + 
+    			 "sdtm:sex 					?sex . \n" + 
+			 "[		a	       sdtm:ConcomitantMedication ; \n" + 
+				 "sdtm:subject   ?patient ; \n" + 
+				 "sdtm:standardizedMedicationName	 ?takes ; \n" + 
+				 "spl:activeIngredient [ spl:classCode ?ingred ] ; \n" + 
+				 "sdtm:startDateTimeOfMedication ?start \n" + 
+    		  "] . \n" + 
+			 "} WHERE { \n" + 
+			 "?patient \n" + 
+    			 "a					  hl7:Person ; \n" + 
+    			 "hl7:entityName			  ?middleName ; \n" + 
+    			 "hl7:livingSubjectBirthTime		  ?dob ; \n" + 
+    			 "hl7:administrativeGenderCodePrintName ?sex ; \n" + 
+    			 "hl7:substanceAdministration		  [ \n" + 
+				 "a	       hl7:SubstanceAdministration ; \n" + 
+ 				 "hl7:consumable [ \n" + 
+	    			 "hl7:displayName	 ?takes ; \n" + 
+	    			 "spl:activeIngredient [ \n" + 
+					 "spl:classCode ?ingred \n" + 
+	    			 "] \n" + 
+				 "] ; \n" + 
+				 "hl7:effectiveTime [ \n" + 
+	    			 "hl7:start ?start \n" + 
+				 "] \n" + 
+    			 "] . \n" + 
+			 "} \n" ; 
+    alert(s);
+	return s;
+}
+
+function getDB_HL7()
+{
+	var s = "";
+		s += "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \n" +
+			 "PREFIX Person: <http://hospital.example/DB/Person#> \n" +
+			 "PREFIX Sex_DE: <http://hospital.example/DB/Sex_DE#>\n" +
+			 "PREFIX Item_Medication: <http://hospital.example/DB/Item_Medication#> \n" +
+			 "PREFIX Medication: <http://hospital.example/DB/Medication#> \n" +
+			 "PREFIX Medication_DE: <http://hospital.example/DB/Medication_DE#> \n" +
+			 "PREFIX NDCcodes: <http://hospital.example/DB/NDCcodes#> \n" +
+			 "PREFIX hl7: <http://www.hl7.org/v3ballot/xml/infrastructure/vocabulary/vocabulary#> \n" +
+			 "PREFIX spl: <http://www.hl7.org/v3ballot/xml/infrastructure/vocabulary/vocabulary#> \n" +
+			 "CONSTRUCT { \n" +
+			 "\n" +
+			 "?person \n" +
+			 "    a					  hl7:Person ; \n" +
+			 "    hl7:entityName			  ?middleName ; \n" +
+			 "    hl7:livingSubjectBirthTime		  ?dob ; \n" +
+			 "    hl7:administrativeGenderCodePrintName ?sex ; \n" +
+			 "    hl7:substanceAdministration		  [ \n" +
+			 "	a	       hl7:SubstanceAdministration ; \n" +
+			 " 	hl7:consumable [ \n" +
+			 "	    hl7:displayName	 ?takes ; \n" +
+			 "	    spl:activeIngredient [ \n" +
+			 "		spl:classCode ?ingred \n" +
+			 "	    ] \n" +
+			 "	] ; \n" +
+			 "	hl7:effectiveTime [ \n" +
+			 "	    hl7:start ?indicDate \n" +
+			 "	    # hl7:end   (?indicDate + ?indicDuration) \n" +
+			 "	] \n" +
+			 "    ] . \n" +
+			 "} WHERE { \n" +
+			 "  ?person     Person:MiddleName		     ?middleName ; \n" +
+			 "              Person:DateOfBirth	     ?dob ; \n" +
+			 "              Person:SexDE		     ?sexEntry   . \n" +
+			 " \n" +
+			 "  OPTIONAL { \n" +
+			 "              ?sexEntry   Sex_DE:EntryName   ?sex . \n" +
+			 "  } \n" +
+			 "  OPTIONAL { \n" +
+			 "  ?indicItem  Item_Medication:PatientID	     ?person ; \n" +
+			 "              Item_Medication:PerformedDTTM  ?indicDate ; \n" +
+			 "              Item_Medication:EntryName	     ?takes . \n" +
+			 "  ?indicMed   Medication:ItemID		     ?indicItem ; \n" +
+			 "              Medication:DaysToTake	     ?indicDuration ; \n" +
+			 "              Medication:MedDictDE	     ?indicDE . \n" +
+			 "  ?indicDE    Medication_DE:NDC		     ?indicNDC . \n" +
+			 "  } \n" +
+			 " \n" +
+			 "  OPTIONAL { \n" +
+			 "  ?indicCode  NDCcodes:NDC         ?indicNDC ; \n" +
+			 "              NDCcodes:ingredient  ?ingred . \n" +
+			 "  } \n" +
+			 "} LIMIT 30 \n" ;
+			 
+    alert(s);
+	return s;
+	
+
+}
+
+
 function getResults()
 {
    //transform query and get patient list coi database 
 	try {
+		
+		
 		showWait(true);
-  		process(host + path + "/swoPatient?", function(n3) {
+  		
+  		var query = host + path + "/swoPatient?";
+  			query += "sdtm=" + encodeURIComponent("http://localhost/.context+" + encodeURIComponent(getSDTM()));
+ 		    query += "&&hl7_sdtm=" + encodeURIComponent("http://localhost/.context+" + encodeURIComponent(getHL7_SDTM()));
+  		    query += "&&db_hl7=" + encodeURIComponent("http://localhost/.context+" +  encodeURIComponent(getDB_HL7()));
+  		
+  		process(query, function(n3) {
            //alert(n3);
           
            var patientlist = []; 
