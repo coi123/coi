2d391f69cf09c35036906fe09b7041b881b7f377
holger.stenzhorn <holger.stenzhorn@b57e6fc4-c6dd-11dd-a3c7-fda619b15ae3>
2009-02-09 18:10:08 +0000

    - Removed wrong characters at beginning of lines - Added test for existence of temp dir - Parametrized http://hospital.example/DB/

--- bin/swo.bat
+++ bin/swo.bat
@@ -3,16 +3,18 @@ set dir=%~dp0
 set temp=%dir%temp\
 for /f "tokens=1,2,3,4,5,6,7,8 delims=.:,\ " %%a in ("%date% %time% %random%") do set "suffix=%%c%%b%%a-%%d%%e%%f%%g-%%h"
 
-\set one=%temp%sdtm-%suffix%.rq 
-\set two=%temp%hl7_sdtm-%suffix%.rq 
-\set three=%temp%db_hl7-%suffix%.rq
+if not exist %temp% mkdir %temp%
 
-\wget -nv -O %one% %1
-\wget -nv -O %two% %2
-\wget -nv -O %three% %3
+rem set one=%temp%sdtm-%suffix%.rq 
+rem set two=%temp%hl7_sdtm-%suffix%.rq 
+rem set three=%temp%db_hl7-%suffix%.rq
 
-\%dir%SWtransformer -q %one% %two% | %dir%SWtransformer -q - %three% -s http://hospital.example/DB/ | mysql -u %4 --password=%5 %6 --table
+rem wget -nv -O %one% %1
+rem wget -nv -O %two% %2
+rem wget -nv -O %three% %3
 
-\del %one%
-\del %two%
-\del %three%
+rem %dir%SWtransformer -q %one% %two% | %dir%SWtransformer -q - %three% -s http://hospital.example/DB/ | mysql -u %4 --password=%5 %6 --table
+
+rem del %one%
+rem del %two%
+rem del %three%
