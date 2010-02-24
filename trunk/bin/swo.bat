698992d9285519864b400aa7004f2d8ff50b7942
holger.stenzhorn <holger.stenzhorn@b57e6fc4-c6dd-11dd-a3c7-fda619b15ae3>
2010-02-24 13:46:23 +0000

    Replace SWtransformer with new SPARQL binary and changed batch scripts accordingly

--- trunk/bin/swo.bat
+++ trunk/bin/swo.bat
@@ -13,7 +13,7 @@ wget -nv -O %one% %1
 wget -nv -O %two% %2
 wget -nv -O %three% %3
 
-%dir%SWtransformer -q %one% %two% | %dir%SWtransformer -q - %three% -s %4 | mysql -u %5 --password=%6 %7 --table
+%dir%SPARQL -p %one% %two% | %dir%SPARQL -p - %three% -s %4 | mysql -u %5 --password=%6 %7 --table
 
 del %one%
 del %two%
